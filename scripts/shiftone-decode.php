0f994fc8d5d742e89f6ac82af4f9b30f5f202c09
jamp-shareable-scripts <37050900+jamp-shareable-scripts@users.noreply.github.com>
2020-04-06 14:29:05 +0200

    Create shiftone-decode.php

---
+++ scripts/shiftone-decode.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Shift-one decodes a string.
+ * 
+ * Usage: jamp shiftone-decode <input>
+ *
+ * @author  jamp-shareable-scripts <https://github.com/jamp-shareable-scripts>
+ * @license GPL-2.0
+ */
+jampUse('jampEcho');
+if (empty($argv[1])) {
+	passthru('jamp usage shiftone-decode');
+	exit;
+}
+
+function shiftoneDecode($input) {
+	$shiftedCharCodes = is_array($input) ? $input : explode(',', $input);
+	$output = '';
+	if (function_exists('mb_chr')) {
+		foreach ($shiftedCharCodes as $charCode) {
+			if (!is_numeric($charCode)) {
+				throw new Error('Non-numeric value encountered.');
+			}
+			$code = intval($charCode);
+			$output .= mb_chr($code >> 1);
+		}
+	}
+	else {
+		foreach ($shiftedCharCodes as $charCode) {
+			if (!is_numeric($charCode)) {
+				throw new Error('Non-numeric value encountered.');
+			}
+			$code = intval($charCode);
+			$output .= chr($code >> 1);
+		}
+	}
+	return $output;
+}
+
+$input = NULL;
+if ($argv[2]) {
+	$input = array_slice($argv, 1);
+}
+else {
+	$input = $argv[1];
+}
+
+jampEcho(shiftoneDecode($input));
